2613faef97b08aae4eb5afdc2f933c094fa38852
Austin Ros <austinros@mac.mynetworksettings.com>
2026-03-12 14:02:47 -0400

    Increase startup market limit to 10

--- run_paper_bot.sh
+++ run_paper_bot.sh
@@ -5,7 +5,7 @@ cd /opt/binary-bot/meta-bot
 source venv/bin/activate
 
 export POLYMARKET_MODE=live
-export POLYMARKET_MARKET_LIMIT=1
+export POLYMARKET_MARKET_LIMIT=10
 export POLYMARKET_DEBUG_RAW=false
 export APIFOOTBALL_API_KEY="2a0966f08995b9ea7235261cdf039e76"
 
