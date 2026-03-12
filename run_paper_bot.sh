e11f4957025bfecc0f798e3d18a06c89a499e52b
Austin Ros <austinros@mac.mynetworksettings.com>
2026-03-12 13:57:23 -0400

    Rebuild market map before bot startup

--- run_paper_bot.sh
+++ run_paper_bot.sh
@@ -7,5 +7,10 @@ source venv/bin/activate
 export POLYMARKET_MODE=live
 export POLYMARKET_MARKET_LIMIT=1
 export POLYMARKET_DEBUG_RAW=false
+export APIFOOTBALL_API_KEY="2a0966f08995b9ea7235261cdf039e76"
 
+echo "[startup] rebuilding market map"
+python3 -m binary_bot.build_market_map
+
+echo "[startup] launching bot app"
 exec python3 -m binary_bot.app
